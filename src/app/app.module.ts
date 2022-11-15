c7e336ebe630e7644d0b5b7580e900eb8469351e
Muhammad Mohsin1 <muhammad.mohsin1@dmifinance.in>
2022-11-15 10:10:47 +0530

    material forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterilaModule } from './materila/materila.module';
+import {MatNativeDateModule} from '@angular/material/core';
+import { ReactformComponent } from './reactform/reactform.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReactformComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterilaModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatNativeDateModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/reactform/reactform.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+interface Relegion{
+  value: string;
+  viewValue: string;
+}
+
+@Component({
+  selector: 'app-reactform',
+  templateUrl: './reactform.component.html',
+  styleUrls: ['./reactform.component.css']
+})
+
+export class ReactformComponent implements OnInit {
+
+  title = "Reactive Form";
+  relegion: Relegion[] = [
+    {'value':'hindu', 'viewValue':'Hindu'},
+    {'value':'muslim', 'viewValue':'Muslim'},
+    {'value':'sikh', 'viewValue':'Sikh'},
+    {'value':'christian', 'viewValue':'Christian'}
+  ];
+
+  userDetail = {
+  firstName:"Mohsin",
+  lastName:"Gaur",
+  gender: "male",
+  rel: "muslim"
+  }
+
+  userForm = new FormGroup({
+    firstName: new FormControl('',[Validators.required]),
+    lastName: new FormControl('',[Validators.required]),
+    gender: new FormControl('',[Validators.required]),
+    rel: new FormControl('',[Validators.required])
+  });
+
+  saveData(){
+    if(this.userForm.valid){
+      console.log("User Information ",this.userForm.value);
+    } else{
+      console.log("Some fields missing!.");
+    }
+  }
+
+  constructor() { 
+    this.userForm.patchValue(this.userDetail);
+  }
+
+  ngOnInit(): void {
+  }
+
+}
